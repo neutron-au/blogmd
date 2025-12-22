db0cae77599fd0416bca0f2acde20a98ed130700
Mitch Naake <mitch.naake@health.wa.gov.au>
2025-12-22 09:40:31 +0800

    add dynamic file/blog service based on url/endpoint.

--- src/mdserver/server.py
+++ src/mdserver/server.py
@@ -1,9 +1,8 @@
 import os
 import flask
-from .util import generate_page
+from .util import generate_page, send_file_raw
 
 
-    
 def get_file(filename):
     # return 404 if file doesn't exist
     if f'{filename}' not in os.listdir('file/') : return generate_page('error/404.md'), 404
@@ -14,14 +13,30 @@ def start(host:str='0.0.0.0', port:int=8080, debug:bool=False) -> flask.Flask:
     app = flask.Flask('markdown-server')
     app.url_map.strict_slashes = False
 
-    @app.route('/<branch>/<name>/', methods=['GET'])
-    def blog_view(branch, name):
-        if branch.lower() == 'file' : return get_file(name), 200
-        
-        if f'{name}.md' not in os.listdir(f'{branch}/') : return generate_page('error/404.md'), 404
-        
-        return generate_page(f'{branch}/{name}.md'), 200
+    # /
+    @app.route('/', methods=['GET'])
+    def root() : return '/blog/', 304
 
+    # /blog/
+    @app.route('/blog/', methods=['GET'])
+    def blog_root() : return '/blog/example-md', 304
 
+    @app.route('/<path:branch>/', methods=['GET'])
+    def get_file(branch):
+        #
+        # 
+        #
+        split_branch = list(filter(None, branch.split('/')))
+        domain = split_branch[0]
+        print(f'domain="{domain}", split_branch={split_branch}')
+        print(domain)
+        if domain == 'file':
+            path = os.path.join(*split_branch)
+            print(path)
+            return send_file_raw(path)
+            
+        if domain == 'blog':
+            split_branch[-1] = split_branch[-1] + '.md'
+            return generate_page(os.path.join(*split_branch)) 
 
     app.run(host, port, debug)
--- src/mdserver/util.py
+++ src/mdserver/util.py
@@ -1,9 +1,13 @@
+import os
 import jinja2
 import markdown
+import flask
 
+CONTENT_DIR = 'content'
 
 def get_template(path:str=None) -> jinja2.Template:
     if path == None : raise Exception('path value must not be None')
+    path = os.path.join(CONTENT_DIR, path)
     with open(path, "r+") as file:
         content = file.read()
         file.close()
@@ -13,15 +17,20 @@ def markdown_to_html(content:str=None):
     if content == None : raise Exception('content value must not be None')
     return markdown.markdown(content, extensions=['tables'])
 
-blog_template = get_template('file/view_blog.html')
+def send_file_raw(path:str=None):
+    path = os.path.join(CONTENT_DIR, path)
+    return flask.send_file(path)
 
 
-def generate_page(md_path:str=None):
-    with open(md_path, 'r+') as file:
-        blog_md = file.read()
-        file.close()
-
+def generate_page(path:str=None):
+    try:
+        path = os.path.join(CONTENT_DIR, path)
+        print(path)
+        with open(path, 'r+') as file:
+            blog_md = file.read()
+            file.close()
         blog_md_html = markdown_to_html(blog_md)
         blog_template = get_template('file/view_blog.html')
-
         return blog_template.render(blog_html=blog_md_html)
+    except FileNotFoundError:
+        return generate_page('error/404.md')
